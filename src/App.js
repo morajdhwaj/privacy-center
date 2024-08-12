5fd1f4bd670856e96048ccae7afb149105eecba8
Moraj <109418239+morajdhwaj@users.noreply.github.com>
2024-08-12 19:01:11 +0530

    Update

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import { Route, Routes } from 'react-router-dom';
-import './App.scss';
-import HomePage from './pages/home-page/HomePage';
-import RequestPage from './pages/request-page/RequestPage';
-import Navbar from './components/navbar/Navbar';
-import Preference from './pages/preference-page';
-import DparWeb from './pages/dpar-web';
-import PreferenceLogin from './components/preference-login';
+import { Route, Routes } from "react-router-dom";
+import "./App.scss";
+import HomePage from "./pages/home-page/HomePage";
+import RequestPage from "./pages/request-page/RequestPage";
+import Navbar from "./components/navbar/Navbar";
+import Preference from "./pages/preference-page";
+import DparWeb from "./pages/dpar-web";
+import PreferenceLogin from "./components/preference-login";
 
 function App() {
   return (
     <div>
       <Navbar />
       <Routes>
-        <Route path='/' element={<HomePage />} />
-        <Route path='/request' element={<RequestPage />} />
-        <Route path='/preference' element={<Preference />} />
-        <Route path='/preference-login' element={<PreferenceLogin />} />
-        <Route path='/dparwebform' element={<DparWeb />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/request" element={<RequestPage />} />
+        <Route path="/preference" element={<Preference />} />
+        <Route path="/preference-login" element={<PreferenceLogin />} />
+        <Route path="/dparwebform" element={<DparWeb />} />
       </Routes>
     </div>
   );
