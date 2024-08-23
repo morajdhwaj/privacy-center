598c90064c8edb7db6494f30b74bf9d643d392b4
Moraj <109418239+morajdhwaj@users.noreply.github.com>
2024-08-23 15:26:00 +0530

    Add perfios-preview.png for logo

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,28 +1,28 @@
-import React from 'react';
-import './_navbar.scss';
-import { Button } from '@carbon/react';
+import React from "react";
+import "./_navbar.scss";
+import { Button } from "@carbon/react";
 
 const Navbar = () => {
   return (
-    <div className='navbar'>
-      <div className='navbar-div'>
-        <div className='navbar-div-div1'>
-          <a href='/'>
+    <div className="navbar">
+      <div className="navbar-div">
+        <div className="navbar-div-div1">
+          <a href="/">
             <img
-              src='./images/concur-logo.png'
-              alt=''
-              className='navbar-div-div1-img'
+              src="./images/perfios-preview.png"
+              alt=""
+              className="navbar-div-div1-img"
             />
           </a>
         </div>
-        <div className='navbar-div-div2'>
-          <Button kind='ghost' href='/request'>
+        <div className="navbar-div-div2">
+          <Button kind="ghost" href="/request">
             View Past Request
           </Button>
-          <Button kind='danger' href='/dparwebform'>
+          <Button kind="danger" href="/dparwebform">
             Make a Privacy Request
           </Button>
-          <Button kind='ghost' href='/preference-login'>
+          <Button kind="ghost" href="/preference-login">
             Preference
           </Button>
         </div>
